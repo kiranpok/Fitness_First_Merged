ecce4f85b4b24c33d3d4f650a21f79f1a19e322c
Kiran Pokharel <112083483+kiranpok@users.noreply.github.com>
2024-04-22 15:49:45 +0300

    Update ActivityStats.js: Deleted unused variables

--- frontend/src/components/ActivityStats.js
+++ frontend/src/components/ActivityStats.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { useRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-import Chart from "chart.js/auto";
 import { useTranslation } from "react-i18next";
 
 
